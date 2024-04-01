01a8f3967f2f6989b5ab3e1cc009883d008261e7
johanp0308 <sebastian-johan1@hotmail.com>
2024-04-01 06:50:20 -0500

    feat: :tada: Init commit

---
+++ src/main/java/com/campus/spring/youropinion/YouropinionApplication.java
@@ -0,0 +1,13 @@
+package com.campus.spring.youropinion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YouropinionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YouropinionApplication.class, args);
+	}
+
+}
