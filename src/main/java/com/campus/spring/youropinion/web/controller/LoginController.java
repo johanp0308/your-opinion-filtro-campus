0330fe186b0602c7342c645b2c0728c764f0b794
johanp0308 <sebastian-johan1@hotmail.com>
2024-04-01 12:32:27 -0500

    refactor: :recycle: Remove code inncesary

--- src/main/java/com/campus/spring/youropinion/web/controller/LoginController.java
+++ src/main/java/com/campus/spring/youropinion/web/controller/LoginController.java
@@ -17,7 +17,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 
-@RestController
+@RestController 
 @SecurityRequirement(name = "bearerAuth")
 public class LoginController {
     
